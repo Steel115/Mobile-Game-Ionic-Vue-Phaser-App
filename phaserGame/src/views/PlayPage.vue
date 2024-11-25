13d4b2693c099446be18cb6ee71aa81ca2245a63
enriquevmz <evelazquezm11@gmail.com>
2024-11-25 17:59:31 +0000

    ⭐Adjusting Game Window

--- phaserGame/src/views/PlayPage.vue
+++ phaserGame/src/views/PlayPage.vue
@@ -1,20 +1,15 @@
 <template>
-    <ion-page>
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Play</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content :fullscreen="true">
-        <ion-header collapse="condense">
-          <ion-toolbar>
-            <ion-title size="large">Play</ion-title>
-          </ion-toolbar>
-        </ion-header>
-        <PhaserContainer/>
-      </ion-content>
-    </ion-page>
-  </template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Play</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content >
+        <PhaserContainer />
+    </ion-content>
+  </ion-page>
+</template>
   
   <script setup lang="ts">
   import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
