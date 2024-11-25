05a3ac819fba99868bfe62e411aefe94fe6dade8
enriquevmz <evelazquezm11@gmail.com>
2024-11-25 17:40:02 +0000

    ⭐Creating Scenes

--- phaserGame/src/game/game.js
+++ phaserGame/src/game/game.js
@@ -1,32 +1,20 @@
-import { Game, AUTO, Scale, Scene } from "phaser";
+import { Game, AUTO, Scale } from "phaser";
+import { PlayScene } from "./PlayScene.js";
 
 export function launch() {
     return new Game({
-        type: AUTO,
-        scale: {
-            mode: Scale.RESIZE,
-            width: window.innerWidth * window.devicePixelRatio,
-            autoCenter: Scale.CENTER_BOTH,
-            height: window.innerHeight * window.devicePixelRatio,
-        },
-        parent: "game",
-        backgroundColor: "#201726",
-        physics: {
-            default: "arcade",
-        },
-        scene: MainScene,
+      type: AUTO,
+      scale: {
+        mode: Scale.RESIZE,
+        width: window.innerWidth * window.devicePixelRatio,
+        autoCenter: Scale.CENTER_BOTH,
+        height: window.innerHeight * window.devicePixelRatio,
+      },
+      parent: "game",
+      backgroundColor: "#201726",
+      physics: {
+        default: "arcade",
+      },
+      scene: PlayScene,
     });
-}
-
-export class MainScene extends Scene {
-    constructor() {
-        super({ key: 'MainScene' });
-    }
-
-    create() {
-        this.add.text(100, 100, "Hello Phaser!", {
-            font: "24px Courier",
-            fill: "#ffffff",
-        });
-    }
-}
+  }
